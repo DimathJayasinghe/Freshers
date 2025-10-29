f957fbf9513b29c9d9aeb07b6e130d035cf4f154
DimathJayasinghe <dimathjayasinghe@gmail.com>
2025-10-29 19:56:24 +0530

    almost fixed

--- eslint.config.js
+++ eslint.config.js
@@ -19,5 +19,9 @@ export default defineConfig([
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    parserOptions: {
+      tsconfigRootDir: __dirname,
+      project: ['./tsconfig.json'],
+    },
   },
 ])
